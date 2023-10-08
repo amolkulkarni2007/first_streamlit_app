8f57b3b70ce5ad79414e6b4b70b9319b5d891ca6
amolkulkarni2007 <45559874+amolkulkarni2007@users.noreply.github.com>
2023-10-08 17:37:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
-streamlit.title("For Snowflake")
+streamlit.title("My parents new healthy Diner")
+
+streamlt.header('Breakfast Menu')
+streamlt.text('Omlete')
+streamlt.text('Smoothie')
+streamlt.text('Egg')
+
