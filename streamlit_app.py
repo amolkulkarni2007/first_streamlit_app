dae9313000198a276de6ca3db072906cf991d140
amolkulkarni2007 <45559874+amolkulkarni2007@users.noreply.github.com>
2023-10-12 21:15:33 +0530

    Update streamlit_app.py
    requests corrected

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,6 @@ streamlit.dataframe(fruis_to_show)
 
 
 #New section to display fruityvice api response
-import request
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
